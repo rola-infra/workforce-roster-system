5a01843bff974b1f184f8aad61849e384bbc0737
rola-infra <rolaregex@gmail.com>
2026-06-30 01:28:28 +0530

    fix: move server.js next to package.json, fix import and dotenv paths

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-import app from './app.js';
-import connectDB from './config/db.js';
+import app from './src/app.js';
+import connectDB from './src/config/db.js';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './src/config/.env' });
